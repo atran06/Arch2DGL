00507912c5e7af8475f7686031cb829990cc773f
Arthur Tran <tran.art13@gmail.com>
2018-06-25 19:21:33 -0700

    Added color

--- src/com/arthurtran/Arch2DGL/main/A2D.java
+++ src/com/arthurtran/Arch2DGL/main/A2D.java
@@ -5,4 +5,12 @@ public class A2D {
     public static int A2D_CENTERED = 151;
     public static int A2D_TRUE = 1;
     public static int A2D_FALSE = 0;
+    public static int A2D_RED = 255000000;
+    public static int A2D_BLUE = 255;
+    public static int A2D_GREEN = 255000;
+    public static int A2D_CYAN = 255255;
+    public static int A2D_MAGENTA = 255000255;
+    public static int A2D_YELLOW = 255255000;
+    public static int A2D_BLACK = 255255255;
+    public static int A2D_WHITE = 0;
 }
--- src/com/arthurtran/Arch2DGL/main/Renderer.java
+++ src/com/arthurtran/Arch2DGL/main/Renderer.java
@@ -3,11 +3,8 @@ package com.arthurtran.Arch2DGL.main;
 import com.arthurtran.Arch2DGL.shape.Shape;
 import com.arthurtran.Arch2DGL.window.Window;
 import org.lwjgl.glfw.GLFW;
-
-import static org.lwjgl.glfw.GLFW.GLFW_SAMPLES;
-import static org.lwjgl.opengl.ARBImaging.glBlendEquation;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL14.GL_FUNC_ADD;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 
 public class Renderer {
 
@@ -25,16 +22,40 @@ public class Renderer {
         GLFW.glfwPollEvents();
     }
 
-    public void setAA() {
-        GLFW.glfwWindowHint(GLFW_SAMPLES, 4);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glEnable(GL_BLEND);
-        glBlendEquation(GL_FUNC_ADD);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_LINE_SMOOTH);
-        glEnable(GL_POINT_SMOOTH);
-        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-        glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
+    public void enableBlending() {
+        GL11.glEnable(GL11.GL_BLEND);
+        GL14.glBlendEquation(GL14.GL_FUNC_ADD);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    public void enableAA() {
+        GLFW.glfwWindowHint(GLFW.GLFW_SAMPLES, 4);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_POINT_SMOOTH);
+        GL11.glEnable(GL11.GL_POLYGON_SMOOTH);
+        GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_NICEST);
+        GL11.glHint(GL11.GL_POINT_SMOOTH_HINT, GL11.GL_NICEST);
+        GL11.glHint(GL11.GL_POLYGON_SMOOTH_HINT, GL11.GL_NICEST);
+    }
+
+    public void setColor(int color, float alpha) {
+        if(color == A2D.A2D_RED) GL11.glColor4f(1, 0, 0, alpha);
+        if(color == A2D.A2D_GREEN) GL11.glColor4f(0, 1, 0, alpha);
+        if(color == A2D.A2D_BLUE) GL11.glColor4f(0, 0, 1, alpha);
+        if(color == A2D.A2D_CYAN) GL11.glColor4f(0, 1, 1, alpha);
+        if(color == A2D.A2D_MAGENTA) GL11.glColor4f(1, 0, 1, alpha);
+        if(color == A2D.A2D_YELLOW) GL11.glColor4f(1, 1, 0, alpha);
+        if(color == A2D.A2D_BLACK) GL11.glColor4f(0, 0, 0, alpha);
+        if(color == A2D.A2D_WHITE) GL11.glColor4f(1, 1, 1, alpha);
+    }
+
+    public void setColorRGB(float r, float g, float b) {
+        GL11.glColor3f(r / 255, g / 255, b / 255);
+    }
+
+    public void setColorRGBA(float r, float g, float b, float alpha) {
+        GL11.glColor4f(r / 255, g / 255, b / 255, alpha);
     }
 
     public void draw(Shape shape) {
